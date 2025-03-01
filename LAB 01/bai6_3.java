e47e8c6c848f89c17d7c9a196c5a166e2f7a803b
MiniBox1508 <88941861+MiniBox1508@users.noreply.github.com>
2025-03-01 21:59:39 +0700

    Create bai6_3.java

---
+++ LAB 01/bai6_3.java
@@ -0,0 +1,17 @@
+package Ex6_3;
+import java.util.Scanner;
+public class Ex6_3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int x = 0; x < 2*i - 1; x++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

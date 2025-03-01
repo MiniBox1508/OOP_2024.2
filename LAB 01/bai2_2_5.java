028115ba8b2db5dcdb145b84ab96dd3c03ac29a0
MiniBox1508 <88941861+MiniBox1508@users.noreply.github.com>
2025-03-01 22:09:32 +0700

    Create bai2_2_5.java

---
+++ LAB 01/bai2_2_5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ex2_2_5 {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        double a = sc.nextDouble();
+	        double b = sc.nextDouble();
+	        System.out.println("Sum = " + (a + b));
+	        System.out.println("Difference = " + (a - b));
+	        System.out.println("Product = " + (a * b));
+	        System.out.println("Quotient = " + (a / b));}
+}

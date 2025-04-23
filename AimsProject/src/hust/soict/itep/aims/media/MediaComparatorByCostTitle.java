3c36b73ceb485be79ff5fe158dd4c2422fa08cbf
MiniBox1508 <88941861+MiniBox1508@users.noreply.github.com>
2025-04-23 23:41:57 +0700

    Update MediaComparatorByCostTitle.java

--- AimsProject/src/hust/soict/itep/aims/media/MediaComparatorByCostTitle.java
+++ AimsProject/src/hust/soict/itep/aims/media/MediaComparatorByCostTitle.java
@@ -3,7 +3,6 @@ package hust.soict.itep.aims.media;
 import java.util.Comparator;
 
 public class MediaComparatorByCostTitle implements Comparator<Media> {
-    @Override
     public int compare(Media m1, Media m2) {
         int costCompare = Float.compare(m2.getCost(), m1.getCost());
         if (costCompare != 0) {

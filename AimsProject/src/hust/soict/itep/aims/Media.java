403a4e2b91ee7b1cefe8498bdcf2954c1e889edb
MiniBox1508 <88941861+MiniBox1508@users.noreply.github.com>
2025-04-23 18:50:57 +0700

    Add files via upload

---
+++ AimsProject/src/hust/soict/itep/aims/Media.java
@@ -0,0 +1,63 @@
+package hust.soict.itep.aims.media;
+
+import java.util.Comparator;
+
+public abstract class Media {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private static int nbMedia = 0;
+
+    public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost();
+    public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
+
+    public Media(String title) {
+        this.title = title;
+        this.id = ++nbMedia;
+    }
+
+    public Media(String title, String category, float cost) {
+        this(title);
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Media)) return false;
+        Media other = (Media) obj;
+        return title != null && title.equals(other.title);
+    }
+
+    public abstract boolean isMatch(String title);
+}

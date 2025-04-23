cd70ea8031710d3607e5d7f0782180047abc0c1d
MiniBox1508 <88941861+MiniBox1508@users.noreply.github.com>
2025-04-23 18:56:10 +0700

    Add files via upload

---
+++ AimsProject/src/hust/soict/itep/aims/TestPassingParameter.java
@@ -0,0 +1,26 @@
+package hust.soict.itep.aims;
+
+import hust.soict.itep.aims.media.DigitalVideoDisc;
+
+public class TestPassingParameter {
+    public static void main(String[] args) {
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        String tempTitle = dvd1.getTitle();
+        dvd1.setTitle(dvd2.getTitle());
+        dvd2.setTitle(tempTitle);
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        dvd.setTitle(title);
+    }
+}
